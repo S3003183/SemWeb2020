b0aecc9eab2735cba9474c7efab939c7a8e10e0c
Andris Jakubovskis <a.jakubovskis@student.rug.nl>
2020-11-07 20:42:38 +0100

    Query musicbrainz & wikidata

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import pandas as pd
 
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+def run_sparql_query(url, query, return_format):
+    sparql = SPARQLWrapper(url)
 
-sparql.setQuery("""
-SELECT ?releasetype ?releasetypeLabel
-WHERE {?releasetype wdt:P279 wd:Q2431196 .
-       service wikibase:label { bd:serviceParam wikibase:language "en". }
-      }
+    sparql.setQuery(query)
+    sparql.setReturnFormat(return_format)
+    return sparql.query().convert()
 
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
 
-results_df = pd.json_normalize(results['results']['bindings'])
-print(results_df[['releasetype.value', 'releasetypeLabel.value']])
+
+wikidata_url = "https://query.wikidata.org/sparql"
+wikidata_query = """
+    SELECT ?releasetype ?releasetypeLabel
+    WHERE {?releasetype wdt:P279 wd:Q2431196 .
+           service wikibase:label { bd:serviceParam wikibase:language "en". }
+          }
+
+    """
+
+# mo_url = "http://dbtune.org/bbc/peel/sparql/"
+mo_url = "http://dbtune.org/musicbrainz/sparql"
+# mo_url = "http://dbtune.org/musicbrainz/snorql/"
+mo_query = """
+SELECT DISTINCT ?class
+WHERE { [] a ?class }
+ORDER BY ?class
+    """
+
+wikidata_results = run_sparql_query(url = wikidata_url, query=wikidata_query, return_format=JSON)
+mo_results = run_sparql_query(url = mo_url, query=mo_query, return_format=JSON)
+
+results_df = pd.json_normalize(mo_results['results']['bindings'])
+print(results_df[['class.value']])
