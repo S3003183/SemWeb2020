604268a6df949b93892279bea1039083b2c43900
Andris Jakubovskis <a.jakubovskis@student.rug.nl>
2020-11-08 21:23:49 +0100

    Add update Wikidate script

--- get-wikidata-instances.py
+++ get-wikidata-instances.py
@@ -1,24 +1,36 @@
-from wikidataintegrator import wdi_core
+from wikidataintegrator import wdi_core, wdi_login
 
 MUSICIAN_ID = 'Q639669'
+INSTANCE_OF_ID = 'P31'
 
 
-def get_list_of_instances(entity_name):
-    item_id = wdi_core.WDItemEngine.get_wd_search_results(entity_name)
-    information = wdi_core.WDItemEngine(wd_item_id=item_id[0])
-    # property_list = information.get_property_list()
-    list_of_instances = []
+def get_instanceOf_ids(entity):
+    ids = []
+    for i in entity.wd_json_representation['claims'][INSTANCE_OF_ID]:
+        ids.append(i['mainsnak']['datavalue']['value']['id'])
 
-    for i in information.wd_json_representation['claims']['P31']:
-        # print(i['mainsnak']['datavalue']['value']['id'])
-        list_of_instances.append(i['mainsnak']['datavalue']['value']['id'])
+    return ids
 
-    return list_of_instances
 
 def main():
-    pierre_michelot = get_list_of_instances("Pierre Michelot")
-    if MUSICIAN_ID not in pierre_michelot:
+    search_results = wdi_core.WDItemEngine.get_wd_search_results("Pierre Michelot")
+    entity = wdi_core.WDItemEngine(wd_item_id=search_results[0])
+    existing_class_ids = get_instanceOf_ids(entity)
+
+    if MUSICIAN_ID not in existing_class_ids:
         print("Not an instance of musician.")
+        login_instance = wdi_login.WDLogin(user='SemWeb2020', pwd='nestor2020')
+
+        data = []
+        for class_id in existing_class_ids:
+            data.append(wdi_core.WDItemID(value=class_id, prop_nr=INSTANCE_OF_ID))
+
+        data.append(wdi_core.WDItemID(value=MUSICIAN_ID, prop_nr=INSTANCE_OF_ID))
+        
+        entity.update(data)
+
+        entity.write(login_instance)
+        print("Entity has been updated.")
 
 if __name__ == "__main__":
     main()
